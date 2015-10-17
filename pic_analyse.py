f945cbdb5ac6de003abab265d3d5c069343fcc68
Abhijith Ramalingam <ajibith1995@gmail.com>
2015-10-17 02:50:33 -0400

    Added K Means Implementation

--- pic_analyse.py
+++ pic_analyse.py
@@ -1,43 +1,104 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.cluster import KMeans
+
+
 im = Image.open("test.jpg")
 pix = im.load()
 x_max = im.size[0]
 y_max = im.size[1]
-R = np.zeros(im.size)
+R = np.zeros((x_max*y_max,3),dtype=np.int)
+G = np.zeros((x_max*y_max,3),dtype=np.int)
+B = np.zeros((x_max*y_max,3),dtype=np.int)
 # print "x_max = {0} y_max = {1}".format(x_max,y_max)
-G = np.zeros(im.size)
-B = np.zeros(im.size)
+# G = np.zeros(im.size)
+# B = np.zeros(im.size)
 
-
+k =0
 for i in range(1,x_max):
 	for j in range(1,y_max):
-		R[i,j] = pix[i,j][0]
-		G[i,j] = pix[i,j][1]
-		B[i,j] = pix[i,j][2]
-		# A[i,j] = pix[i,j][3]
+		R[k,:] = np.array([i,j,pix[i,j][0]]) 
+		G[k,:] = np.array([i,j,pix[i,j][1]]) 
+		B[k,:] = np.array([i,j,pix[i,j][2]]) 
+		k+=1
+		# G[i,j] = pix[i,j][1]
+		# B[i,j] = pix[i,j][2]
+		# # A[i,j] = pix[i,j][3]
+for x in (R,G,B):
+	print "********************"
+	k_means = KMeans(init='k-means++', n_clusters=9, n_init=10)
+	k_means.fit(x)
+	print k_means.cluster_centers_
 
-fig = plt.figure(figsize=(10, 6.4))
+# fignum = 1
+# for name, est in estimators.items():
+#     fig = plt.figure(fignum, figsize=(4, 3))
+#     plt.clf()
+#     ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
 
-ax = fig.add_subplot(221)
-ax.set_title('Red')
-plt.imshow(R)
-ax.set_aspect('equal')
-ax2 = fig.add_subplot(223)
-ax2.set_title('Green')
-plt.imshow(G)
-ax2.set_aspect('equal') 
-ax3 = fig.add_subplot(212)
-ax3.set_title('Blue')
-plt.imshow(B)
-ax3.set_aspect('equal') 
+#     plt.cla()
+#     est.fit(R)
+#     labels = est.labels_
+
+#     ax.scatter(R[:, 0], R[:, 1], R[:, 2], c=labels.astype(np.float))
+
+#     ax.w_xaxis.set_ticklabels([])
+#     ax.w_yaxis.set_ticklabels([])
+#     ax.w_zaxis.set_ticklabels([])
+#     ax.set_xlabel('X')
+#     ax.set_ylabel('Y')
+#     ax.set_zlabel('Z')
+#     fignum = fignum + 1
+
+# # Plot the ground truth
+# fig = plt.figure(fignum, figsize=(4, 3))
+# plt.clf()
+# ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+
+# plt.cla()
+
+# # for name, label in [('Setosa', 0),
+# #                     ('Versicolour', 1),
+# #                     ('Virginica', 2)]:
+#     # ax.text3D(R[y == label, 3].mean(),
+#     #           X[y == label, 0].mean() + 1.5,
+#     #           X[y == label, 2].mean(), name,
+#     #           horizontalalignment='center',
+#     #           bbox=dict(alpha=.5, edgecolor='w', facecolor='w'))
+# # Reorder the labels to have colors matching the cluster results
+# # y = np.choose(y, [1, 2, 0]).astype(np.float)
+# ax.scatter(R[:, 0], R[:, 1], R[:, 2])
+
+# ax.w_xaxis.set_ticklabels([])
+# ax.w_yaxis.set_ticklabels([])
+# ax.w_zaxis.set_ticklabels([])
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# ax.set_zlabel('Z')
+# plt.show()
+
+# fig = plt.figure(figsize=(10, 6.4))
+
+# ax = fig.add_subplot(221)
+# ax.set_title('Red')
+# plt.imshow(R)
+# ax.set_aspect('equal')
+# ax2 = fig.add_subplot(223)
+# ax2.set_title('Green')
+# plt.imshow(G)
+# ax2.set_aspect('equal') 
+# ax3 = fig.add_subplot(212)
+# ax3.set_title('Blue')
+# plt.imshow(B)
+# ax3.set_aspect('equal') 
 
 
-cax = fig.add_axes([0.12, 0.1, 0.78, 0.8])
-cax.get_xaxis().set_visible(False)
-cax.get_yaxis().set_visible(False)
-cax.patch.set_alpha(0)
-cax.set_frame_on(False)
-plt.colorbar()
-plt.show()
+# cax = fig.add_axes([0.12, 0.1, 0.78, 0.8])
+# cax.get_xaxis().set_visible(False)
+# cax.get_yaxis().set_visible(False)
+# cax.patch.set_alpha(0)
+# cax.set_frame_on(False)
+# plt.colorbar()
+# plt.show()
